1da22de841039b380a092a6605e37288883896d3
Abdul Rehman Arif <coolabdulrehman3030@gmail.com>
2023-08-07 18:50:04 +0500

    final commit

---
+++ main2.py
@@ -0,0 +1,37 @@
+import pandas
+
+df = pandas.read_csv("hotels.csv", dtype={'id': str})
+
+
+class Hotel:
+
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df['id'] == self.hotel_id, 'name'].squeeze()
+
+    def book(self):
+        df.loc[df['id'] == self.hotel_id, 'available'] = 'no'
+        df.to_csv('hotels.csv', index=False)
+
+    def available(self):
+        availability = df.loc[df['id'] == self.hotel_id, 'available'].squeeze()
+        if availability == 'yes':
+            return True
+        else:
+            return False
+
+
+class ReservationTicket:
+
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
+
+    def generate(self):
+        content = f'''
+        Thank you for the reservation!
+        Here are your booking data:
+        Name: {self.customer_name}
+        Hotel: {self.hotel.name}
+        '''
+        return content
